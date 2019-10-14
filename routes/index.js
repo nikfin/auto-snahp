e551c4202c00cb2a2e07b18cd44c4675020875b5
Undefined <johann@objekt.stream>
2019-10-14 09:02:01 +0200

    + fixes

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,13 @@ router.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'vue', 'dist', 'index.html'))
 })
 
-router.get('/api/:id', (req, res) => {
+router.get('/api/:name', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
-  const { id } = req.params
-  imdb.get({ id }, { apiKey: 'cc8d6e' }).then(data => {
-    res.json(data)
-  })
+  const { name } = req.params
+  imdb.search({ name }, { apiKey: 'cc8d6e' })
+    .then(data => res.json(data))
 })
 
 module.exports = router
--- vue/src/components/index.vue
+++ vue/src/components/index.vue
@@ -9,9 +9,7 @@
             </h3>
           </div>
           <div>
-            <p class="mb">I scrape IMDB for your lazy ass. Put an IMDB ID in the input below and i will create a html
-              template for
-              your shitty snahp.it post. have fun you fat fuck, suck a dick you pice of shit.</p>
+            <p class="mb">I scrape IMDB for your lazy ass. Put an IMDB ID in the input below and i will create a html template for your shitty snahp.it post. have fun you fat fuck, suck a dick you pice of shit.</p>
             <!-- <vs-input vs-icon="fab fa-imdb" placeholder="IMDB ID" v-model="imdb"/> -->
             <vs-row vs-align="flex-start" vs-type="flex" vs-justify="center" vs-w="12">
               <vs-input vs-icon="local_movies" vs-color="warning" placeholder="IMDB ID" v-model="imdb" />
@@ -28,30 +26,31 @@
 
     <div class="spacer"></div>
 
-    <vs-row v-if="data.title" vs-justify="center">
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="6">
-        <a @click="copyContent()">
+    <vs-row v-if="data" vs-justify="center">
 
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="4"
+        v-for="(result, index) in data.results" :key="index">
+        <a @click="copyContent()">
           <vs-card class="pb h">
             <data>
               <div>
                 <div>
-                  <p align="center"><img :src="data.poster" :title="data.title" width="300" /></p>
+                  <p align="center"><img :src="result.poster" :title="result.title" width="300" /></p>
                 </div><br/>
 
                 <div>
-                  <p align="center"><strong>{{ data.title }} ({{ data.start_year || data.year }})</strong></p>
+                  <p align="center"><strong>{{ result.title }} ({{ result.start_year || result.year }})</strong></p>
                 </div><br/>
               </div>
 
               <div>
-                <div><strong>Summary:</strong> {{ data.plot }}</div><br/>
-                <div><strong>Director:</strong> {{ data.director }}<div/>
-                <div><strong>Stars:</strong> {{ data.actors }}</div>
-                <div><strong>Rating:</strong> {{ data.rating }}/10</div>
-                <div><strong>Votes:</strong> {{ data.votes }}</div>
-                <div><strong>Genres:</strong> {{ data.genres }}</div>
-                <div><strong>Runtime:</strong> {{ data.runtime }}</div>
+                <div><strong>Summary:</strong> {{ result.plot }}</div><br/>
+                <div><strong>Director:</strong> {{ result.director }}</div>
+                <div><strong>Stars:</strong> {{ result.actors }}</div>
+                <div><strong>Rating:</strong> {{ result.rating }}/10</div>
+                <div><strong>Votes:</strong> {{ result.votes }}</div>
+                <div><strong>Genres:</strong> {{ result.genres }}</div>
+                <div><strong>Runtime:</strong> {{ result.runtime }}</div>
               </div>
 
               <div><strong>Links:</strong> <a :href="`https://www.imdb.com/title/${data.imdbid}`">IMDB</a> - <a href="https://pastebin.com/xxxx">NFO</a></div><br/>
@@ -60,8 +59,8 @@
               <a href="https://links.snahp.it/xxxx"><strong>Download from ZIPPYSHARE (Splits) - NO LIMITS</strong></a></div>
             </data>
           </vs-card>
-
         </a>
+
       </vs-col>
     </vs-row>
   </div>
@@ -79,20 +78,12 @@ export default {
     }
   },
   methods: {
-    getImdb: function (id) {
-      if (/^tt\d{7}$/.test(id)) {
-        axios.get(`api/${id}`)
-          .then(res => {
-            console.log(res.data)
-            this.data = res.data
-          })
-      } else {
-        this.$vs.notify({
-          title: 'Error',
-          text: 'invalide IMDB ID',
-          color: 'danger'
+    getImdb: function (search) {
+      axios.get(`api/${search}`)
+        .then(res => {
+          console.log(res.data)
+          this.data = res.data
         })
-      }
     },
 
     copyContent: function () {
